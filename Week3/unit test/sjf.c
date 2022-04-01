44a4f8aa98928aadf2ccabc60c61f1f38358a7ae
Chengming Li <chli1077@colorado.edu>
2022-04-01 12:29:27 -0600

    unit test folder update

--- Week3/unit test/sjf.c
+++ Week3/unit test/sjf.c
@@ -43,7 +43,8 @@ void init(struct mass* ball, int* x_pos,int* y_pos,int*x_vel,int* y_vel,int* mas
 
 void final_velocity(struct mass* ball,int size ){
     for(int i = 0; i< size; i++){
-        ball[i].final_velocity = sqrt(ball[i].x_init_vel*ball[i].x_init_vel* + ball[i].y_init_vel*ball[i].y_init_vel);
+        ball[i].final_velocity = sqrt(ball[i].x_init_vel*ball[i].x_init_vel+ ball[i].y_init_vel*ball[i].y_init_vel);
+        //printf("The final velocity is %d\n", ball[i].final_velocity);
     }   
 }
 
@@ -66,12 +67,12 @@ void y_vel_calc(struct mass* ball, int time,int size){
 
 void x_pos_calc(struct mass* ball, int time,int size){
     for(int i = 0; i< size; i++){
-        ball[i].x_init_pos = ball[i].x_init_pos + (ball[i].x_init_vel*time+0.5*ball[i].acceleration*time*time)
+        ball[i].x_init_pos = ball[i].x_init_pos + (ball[i].x_init_vel*time+0.5*ball[i].acceleration*time*time);
     }
 
 }
 void y_pos_calc(struct mass* ball, int time ,int size){
     for(int i = 0; i< size; i++){
-        ball[i].y_init_pos = ball[i].y_init_pos + (ball[i].y_init_vel*time+0.5*ball[i].acceleration*time*time)
+        ball[i].y_init_pos = ball[i].y_init_pos + (ball[i].y_init_vel*time+0.5*ball[i].acceleration*time*time);
     }
 }
--- Week3/unit test/sjftests.c
+++ Week3/unit test/sjftests.c
@@ -15,17 +15,22 @@ CTEST_SETUP(shortestjobfirst) {
     int x_init_vel[] = {5, 10, 15};
     int y_init_vel[] = {5, 15, 15};
 
-    int mass[] = {1,2,3} 
+    int mass[] = {1,2,3} ;
     data->size = sizeof(x_init_pos) / sizeof(x_init_pos[0]);
 
-    init(data->ball,x_init_pos,y_init_pos,x_init_vel,y_init_vel,data->size);
+    init(data->ball,x_init_pos,y_init_pos,x_init_vel,y_init_vel,mass,data->size);
     final_velocity(data->ball,data->size);
 }
 
-CTEST2(shortestjobfirst, test_process) {
+CTEST2(shortestjobfirst, test_process1) {
     for (int i = 0 ; i<3;i++){
         ASSERT_EQUAL(data->ball[i].mass,i+1);
     }
 }
 
 // TODO add additional tests to help debug
+CTEST2(shortestjobfirst, test_process2) {
+    for (int i = 0 ; i<3;i++){
+        ASSERT_EQUAL(data->ball[i].mass,i+1);
+    }
+}
